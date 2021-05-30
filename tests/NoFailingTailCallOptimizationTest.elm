753c58a285512160820c9a1a8b657fd10bbb4abd
Jeroen Engels <jfm.engels@gmail.com>
2021-05-30 17:51:24 +0200

    Add test for opt out comments

--- tests/NoFailingTailCallOptimizationTest.elm
+++ tests/NoFailingTailCallOptimizationTest.elm
@@ -246,4 +246,13 @@ fun1 n =
 """
                     |> Review.Test.run (rule (optOutWithComment "OPT OUT"))
                     |> Review.Test.expectNoErrors
+        , test "should not report an error when the function body contains the opt out comment" <|
+            \() ->
+                """module A exposing (..)
+fun x =
+  -- OPT OUT
+  fun x + 1
+"""
+                    |> Review.Test.run (rule (optOutWithComment "OPT OUT"))
+                    |> Review.Test.expectNoErrors
         ]
