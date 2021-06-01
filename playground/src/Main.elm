5bf8c6fd447e254db18959ba117d13cc56904347
Jeroen Engels <jfm.engels@gmail.com>
2021-06-01 15:24:04 +0200

    Add lazy view

--- playground/src/Main.elm
+++ playground/src/Main.elm
@@ -2,7 +2,9 @@ module Main exposing (main)
 
 import Browser
 import Html exposing (Html)
+import Html.Attributes as Attr
 import Html.Events as Events
+import Html.Lazy
 
 
 main : Program () Model Msg
@@ -32,6 +34,29 @@ update msg model =
 view : Model -> Html Msg
 view model =
     Html.div []
-        [ Html.button [ Events.onClick Increment ] [ Html.text "+" ]
+        [ Html.h1 [] [ Html.text "Open the console, click on the + button and see what messages are printed" ]
+        , Html.div [] [ Html.text "(Ignore the first things to be printed)" ]
+        , Html.button [ Events.onClick Increment, Attr.style "width" "400px", Attr.style "height" "400px" ] [ Html.text "+" ]
         , Html.div [] [ Html.text (String.fromInt model) ]
+        , workingLazy 1
+        , failingLazy 1 2
         ]
+
+
+viewNothing : String -> a -> Html msg
+viewNothing textToPrint _ =
+    let
+        _ =
+            Debug.log textToPrint ()
+    in
+    Html.text ""
+
+
+workingLazy : a -> Html msg
+workingLazy =
+    Html.Lazy.lazy (viewNothing "SHOULD NOT BE PRINTED")
+
+
+failingLazy : a -> b -> Html msg
+failingLazy a =
+    Html.Lazy.lazy (viewNothing "failingLazy")
