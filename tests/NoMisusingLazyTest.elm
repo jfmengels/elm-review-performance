73bf6a94c03ac5955ccbfc6084a77c1a83097a60
Jeroen Engels <jfm.engels@gmail.com>
2021-06-01 15:53:39 +0200

    Flesh out test case

--- tests/NoMisusingLazyTest.elm
+++ tests/NoMisusingLazyTest.elm
@@ -1,8 +1,16 @@
 module NoMisusingLazyTest exposing (all)
 
 import NoMisusingLazy exposing (rule)
+import Review.Project
 import Review.Test
+import Review.Test.Dependencies
 import Test exposing (Test, describe, test)
+
+
+project : Review.Project.Project
+project =
+    Review.Test.Dependencies.projectWithElmCore
+        |> Review.Project.addDependency Review.Test.Dependencies.elmHtml
 
 
 all : Test
@@ -11,14 +19,17 @@ all =
         [ test "should report an error when encountering Html.lazy with arguments and an unstable reference" <|
             \() ->
                 """module A exposing (..)
-a = 1
+a n =
+  Html.Lazy.lazy helper n
+
+helper _ = text ""
 """
-                    |> Review.Test.run rule
+                    |> Review.Test.runWithProjectData project rule
                     |> Review.Test.expectErrors
                         [ Review.Test.error
-                            { message = "REPLACEME"
+                            { message = "Misuse of a lazy function"
                             , details = [ "REPLACEME" ]
-                            , under = "REPLACEME"
+                            , under = "Html.Lazy.lazy"
                             }
                         ]
         ]
