2ccefca118f7de7f696380187d7d0de5e413c302
Jeroen Engels <jfm.engels@gmail.com>
2021-06-06 11:21:36 +0200

    Add todo

--- src/NoMisusingLazy.elm
+++ src/NoMisusingLazy.elm
@@ -26,6 +26,7 @@ import Set exposing (Set)
 -- TODO Add configuration to specify whether to consider functions as creating new references by default or not, and taking a list of exceptions
 -- TODO Report `Html.Lazy.lazy fn {}`
 -- TODO Consider `Html.Lazy.lazy` created with pipes
+-- TODO Report about unnecessary lazy, when view functions are called with data not from the arguments, and can be extracted to a top-level constant
 
 
 {-| Reports... REPLACEME
