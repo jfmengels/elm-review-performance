fd7c9605f0c0ea43f621626ad25adf3794fea6dd
Jeroen Engels <jfm.engels@gmail.com>
2023-03-19 14:31:19 +0100

    Add tests

--- tests/NoCurryingTest.elm
+++ tests/NoCurryingTest.elm
@@ -8,10 +8,19 @@ import Test exposing (Test, describe, test)
 all : Test
 all =
     describe "NoCurrying"
-        [ test "should report an error when REPLACEME" <|
+        [ test "should not report an error when using functions with all the correct arguments" <|
             \() ->
                 """module A exposing (..)
-a = 1
+a = List.map function list
+function n = n
+"""
+                    |> Review.Test.run rule
+                    |> Review.Test.expectNoErrors
+        , test "should report an error when REPLACEME" <|
+            \() ->
+                """module A exposing (..)
+a = function 0
+function b c = b + c
 """
                     |> Review.Test.run rule
                     |> Review.Test.expectErrors
